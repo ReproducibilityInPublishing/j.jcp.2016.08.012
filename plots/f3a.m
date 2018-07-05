6fccf0915b5f54444aea3eeae5ce7b63d21fdece
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2018-07-05 15:43:25 -0500

    k, plotting

--- plots/f3a.m
+++ plots/f3a.m
@@ -16,7 +16,31 @@ Y=[9.42e-04,
 M=[3.71e-03, 1.85e-03, 9.22e-04, 4.60e-04, 2.30e-04, 1.15e-04;
    9.42e-04, 4.69e-04, 2.34e-04, 1.17e-04, 5.83e-05, 2.92e-05];
 
+s0=2.0;
+a=-0.4;
+x0=[0,4];
+e1=[1,0];
+
+ksq = @(x) sqrt((s0^2) + dot((2*a*e1),(x-x0)));
+k= @(x) ((1/s0)+dot((a*e1),(x-x0)))^(-1)
+
+res=[];
 %Arr=arrayfun(@ksq,X)
 for elem=1:length(X)
-	ksq(X(elem))
+	temp=ksq(X(elem));
+	res(end+1)=temp;
 end
+
+X=[1:4]
+Y=[1:8]
+[x,y]=meshgrid(X,Y)
+res
+%surf(x,y,res)
+
+%random online example
+x=1:0.1:4
+y=1:0.1:4
+[X,Y]=meshgrid(x,y)
+Z=sin(X).^2+cos(Y).^2
+surf(X,Y,Z)
+view(2)
