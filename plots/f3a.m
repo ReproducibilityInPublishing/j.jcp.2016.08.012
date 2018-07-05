457213c82971b4e5b25143500ec8447808f5d01c
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2018-07-05 13:24:23 -0500

    trying arrayfun and for loop

--- plots/f3a.m
+++ plots/f3a.m
@@ -17,4 +17,10 @@ M=[3.71e-03, 1.85e-03, 9.22e-04, 4.60e-04, 2.30e-04, 1.15e-04;
    9.42e-04, 4.69e-04, 2.34e-04, 1.17e-04, 5.83e-05, 2.92e-05];
 
 %ksq(X(1))
-[A]=arrayfun(@ksq,X)
+Arr=arrayfun(@ksq,X)
+Arr
+% for elem=X
+% 	%ksq([elem])
+% 	elem
+% 	(X(1))
+% end
