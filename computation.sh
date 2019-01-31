e1a46d3a238eb92bdfdb8c19e2b67543e343cfdd
adb <adhithyabhaskar@gmail.com>
2019-01-31 16:27:08 -0600

    changed julia path for mac

--- computation.sh
+++ computation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Run experiments
-./julia/bin/julia data/examples/runExperiments.jl $1 | tee results_$1.txt
-
+/Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia data/examples/runExperiments.jl $1 | tee results_$1.txt
 # Check the extracted error results from table
 ./check.sh $1
