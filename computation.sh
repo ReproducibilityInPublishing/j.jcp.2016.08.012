fc19749608a49aee54c5114548dcb285faedaf25
adb <adhithyabhaskar@gmail.com>
2019-02-01 17:26:04 -0600

    added commented mac paths, changed python to python2

--- computation.sh
+++ computation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Run experiments
+# Run experiments\
+# Path for mac: 
 /Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia data/examples/runExperiments.jl $1 | tee results_$1.txt
+#./julia/bin/julia data/examples/runExperiments.jl $1 | tee results_$1.txt
 # Check the extracted error results from table
 ./check.sh $1
--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Simple run to make sure does not crash
+# Path for mac: 
 /Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia data/examples/runfirst.jl
+#./julia/bin/julia data/examples/runfirst.jl
 
 # Run experiments and check output
 ./computation.sh $1
