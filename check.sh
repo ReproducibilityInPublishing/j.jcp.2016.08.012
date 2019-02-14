16696d3a92b570d044056e0cddaf21f9b40eaded
adb <adhithyabhaskar@gmail.com>
2019-02-14 13:41:47 -0600

    code_directory and output

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python2 data/examples/extract_results.py $1 > extracted_results_$1.txt
+python2 code_directory/data/examples/extract_results.py $1 > extracted_results_$1.txt
 diff expected_extracted_results_$1.txt extracted_results_$1.txt
