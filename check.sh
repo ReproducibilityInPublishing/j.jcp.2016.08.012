c5d5aae98b68633026602ab41bf4fe5cbd87bdfb
adb <adhithyabhaskar@gmail.com>
2019-02-17 16:27:29 -0600

    added 3d

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python2 code_directory/data/examples/extract_results.py $1 > extracted_results_$1.txt
-diff expected_extracted_results_$1.txt extracted_results_$1.txt
+python2.7 code_directory/data/examples/extract_results.py $1 > extracted_results_$1.txt
+diff expected_output/expected_extracted_results_$1.txt extracted_results_$1.txt
