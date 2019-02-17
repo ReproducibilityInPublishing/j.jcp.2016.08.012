ec37f82b0f6bc84a6f6551ca92d0021415b9279a
adb <adhithyabhaskar@gmail.com>
2019-02-17 16:27:29 -0600

    3d extraction added

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Simple run to make sure does not crash
+case "$OSTYPE" in
+	linux*) ./julia/bin/julia code_directory/data/examples/runfirst.jl ;;
+    darwin*) /Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia code_directory/data/examples/runfirst.jl ;;
+    win*) %JULIA_HOME%/bin/julia code_directory/data/examples/runfirst.jl ;;
+esac
+
+# Run experiments and check output
+./computation.sh $1
