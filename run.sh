8b2327ebe2abc0bc0dcb554043a21450d03b9024
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-04-24 14:25:58 -0500

    Adjust filepaths and clone commands

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ set -e
 
 # Simple run to make sure does not crash
 case "$OSTYPE" in
-    linux*) ./julia/bin/julia data/examples/runfirst.jl ;;
-    darwin*) /Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia data/examples/runfirst.jl ;;
-    win*) %JULIA_HOME%/bin/julia data/examples/runfirst.jl ;;
+    linux*) ./julia/bin/julia examples/runfirst.jl ;;
+    darwin*) /Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia examples/runfirst.jl ;;
+    win*) %JULIA_HOME%/bin/julia examples/runfirst.jl ;;
 esac
 
 # Run experiments and check output
