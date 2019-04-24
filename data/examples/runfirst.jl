520e9a04b348e99ce80fb76a1c13d6dc032ed387
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-04-24 14:16:46 -0500

    Some changes to runfirst.

--- data/examples/runfirst.jl
+++ data/examples/runfirst.jl
@@ -1,11 +1,14 @@
 ENV["PYTHON"]="/usr/bin/python"
-if Pkg.installed("PyPlot")==nothing
-  Pkg.clone("PyPlot")
+println("Satisfying PyPlot")
+if isdir(Pkg.dir("PyPlot"))==false
+  Pkg.clone("https://github.com/ReproducibilityInPublishing/PyPlot.jl", "PyPlot")
 end
+println("Satisfying jInv")
 if isdir(Pkg.dir("jInv"))==false
-  Pkg.clone("https://github.com/JuliaInv/jInv.jl","jInv")
+  Pkg.clone("https://github.com/ReproducibilityInPublishing/jInv.jl","jInv")
 end
-if isdir(Pkg.dir("FactoredEikonalFastMarching"))==false
-  Pkg.clone("https://github.com/JuliaInv/FactoredEikonalFastMarching.jl","FactoredEikonalFastMarching")
-end
+println("Finished.")
+#if isdir(Pkg.dir("FactoredEikonalFastMarching"))==false
+#  Pkg.clone("https://github.com/JuliaInv/FactoredEikonalFastMarching.jl","FactoredEikonalFastMarching")
+#end
 #Pkg.test("FactoredEikonalFastMarching")
