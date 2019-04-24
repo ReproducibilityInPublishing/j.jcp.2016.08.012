af48935a88dcb5bc2cf19baa243294e0a5db68ba
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-04-24 15:19:10 -0500

    Remove extraneous print statements

--- examples/runfirst.jl
+++ examples/runfirst.jl
@@ -1,13 +1,10 @@
 ENV["PYTHON"]="/usr/bin/python"
-println("Satisfying PyPlot")
 if isdir(Pkg.dir("PyPlot"))==false
   Pkg.clone("https://github.com/ReproducibilityInPublishing/PyPlot.jl", "PyPlot")
 end
-println("Satisfying jInv")
 if isdir(Pkg.dir("jInv"))==false
   Pkg.clone("https://github.com/ReproducibilityInPublishing/jInv.jl","jInv")
 end
-println("Finished.")
 if isdir(Pkg.dir("FactoredEikonalFastMarching"))==false
   Pkg.clone("https://github.com/ReproducibilityInPublishing/j.jcp.2016.08.012","FactoredEikonalFastMarching")
 end
