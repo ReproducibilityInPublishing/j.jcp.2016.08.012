099d08f12cc06583ee841bafef39e0d8f682088f
adb <adhithyabhaskar@gmail.com>
2019-04-25 16:03:38 -0500

    added parameter for 3d and moved location of expected results

--- computation.sh
+++ computation.sh
@@ -2,10 +2,10 @@
 
 # Run experiments\
 case "$OSTYPE" in
-    linux*) ./julia/bin/julia examples/runExperiments.jl $1 | tee results_$1.txt ;;
-    darwin*) /Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia examples/runExperiments.jl $1 | tee results_$1.txt ;;
-    win*) %JULIA_HOME%/bin/julia examples/runExperiments.jl $1 | tee results_$1.txt ;;
+	linux*) ./julia/bin/julia examples/runExperiments.jl $1 $2 | tee results_$1_$2.txt ;;
+    darwin*) /Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia examples/runExperiments.jl $1 $2 | tee results_$1_$2.txt ;;
+    win*) %JULIA_HOME%/bin/julia examples/runExperiments.jl $1 $2 | tee results_$1_$2.txt ;;
 esac
 #./julia/bin/julia data/examples/runExperiments.jl $1 | tee results_$1.txt
 # Check the extracted error results from table
-./check.sh $1
+./check.sh $1 $2
