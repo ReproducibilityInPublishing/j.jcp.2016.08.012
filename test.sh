4e11d6059632e7c6e2a52095a2f0333a7c1aabe5
Rutvik Choudhary <rchoudhary11235@gmail.com>
2019-08-26 14:52:02 -0500

    Removed test file that I forgot to delete

--- test.sh
+++
@@ -1,7 +0,0 @@
-SCRIPT_PATH=$(dirname $(realpath -s $0))
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-echo "script path: $SCRIPT_PATH"
-echo "script dir: $SCRIPT_DIR"
-echo "\$0: $0"
-echo "pwd: $( pwd )"
